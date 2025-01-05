188f399ff866daf9c4ffe64caba1456e6ee9deb5
krsticlazar <159049198+krsticlazar@users.noreply.github.com>
2025-01-05 19:23:25 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,14 +53,13 @@ document.addEventListener("DOMContentLoaded", function() {
         },
     });
 
-    
     // Custom cursor
     const customCursor = document.querySelector('.custom-cursor');
     let cursorX = 0, cursorY = 0;
     let delayX = 0, delayY = 0;
 
     // Funkcija za pomeranje kursora
-    document.addEventListener('mousemove', function (event) {
+    document.addEventListener('mousemove', function(event) {
         cursorX = event.clientX;
         cursorY = event.clientY;
         customCursor.classList.remove('hidden'); // Prikaži krug
@@ -77,8 +76,51 @@ document.addEventListener("DOMContentLoaded", function() {
     followCursor();
 
     // Sakrij krug kada je miš van stranice
-    document.addEventListener('mouseleave', function () {
+    document.addEventListener('mouseleave', function() {
         customCursor.classList.add('hidden');
     });
-});
 
+    // Fade-in efekat za p elemente
+    const paragraphs = document.querySelectorAll("p");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("fade-in");
+                    observer.unobserve(entry.target); // Zaustavlja dalji nadzor za performanse
+                }
+            });
+        },
+        {
+            threshold: 1.0, // Element se mora 100% prikazati u viewportu
+        }
+    );
+
+    paragraphs.forEach(p => {
+        observer.observe(p);
+    });
+
+    // FAQ toggle
+    const faqQuestions = document.querySelectorAll('.faq-question');
+
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', function () {
+            const answer = this.nextElementSibling;
+            const isExpanded = answer.classList.contains('open');
+    
+            // Zatvori sve odgovore
+            faqQuestions.forEach(q => {
+                const ans = q.nextElementSibling;
+                ans.classList.remove('open');
+                q.querySelector('.faq-toggle').textContent = '+';
+            });
+    
+            // Otvori trenutni odgovor ako nije već otvoren
+            if (!isExpanded) {
+                answer.classList.add('open');
+                this.querySelector('.faq-toggle').textContent = '-';
+            }
+        });
+    });
+});
